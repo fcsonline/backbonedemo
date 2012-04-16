70e90dd6b45002508386bd1b3c63f5fd2e050fda
Ferran Basora <fcsonline@gmail.com>
2012-04-17 00:13:38 +0200

    Javascript indent

--- routes/campaign.js
+++ routes/campaign.js
@@ -5,78 +5,78 @@
 
 module.exports = function(app){
 
-    // TODO: Transform to database entity
-    var campaigns = [];
+  // TODO: Transform to database entity
+  var campaigns = [];
 
-    // Templates
-    app.get('/templates/campaign-list', function(req, res){
-      res.render('sections/campaign-list', { title: '' });
+  // Templates
+  app.get('/templates/campaign-list', function(req, res){
+    res.render('sections/campaign-list', { title: '' });
+  });
+
+  app.get('/templates/campaign-list-item', function(req, res){
+    res.render('sections/campaign-list-item', { title: '' });
+  });
+
+  app.get('/templates/campaign-detail', function(req, res){
+    res.render('sections/campaign-detail', { title: '' });
+  });
+
+  app.get('/campaign', function(req, res){
+    console.log("Listing campaigns");
+
+    // TODO: Retrieve from real database
+    campaigns.push({ id : 1, name : 'Campaign 001', description : 'Fusce dapibus, tellus ac cursus commodo, tortor mauris' });
+    campaigns.push({ id : 2, name : 'Campaign 002', description : 'Fusce dapibus, tellus ac cursus commodo, tortor mauris' });
+    campaigns.push({ id : 3, name : 'Campaign 003', description : 'Fusce dapibus, tellus ac cursus commodo, tortor mauris' });
+    campaigns.push({ id : 4, name : 'Campaign 004', description : 'Fusce dapibus, tellus ac cursus commodo, tortor mauris' });
+    campaigns.push({ id : 5, name : 'Campaign 005', description : 'Fusce dapibus, tellus ac cursus commodo, tortor mauris' });
+
+    res.json(campaigns);
+  });
+
+  // Insert a new item
+  app.post('/campaign', function(req, res){
+    console.log("Creating a new campaign");
+
+    // Retrieve form data
+    var name = req.param('name', null);
+    var description = req.param('description', null);
+
+    // TODO: Insert a real database entry
+    campaigns.push({
+      id: campaigns.length + 1,
+      name: name,
+      dscription: description
     });
 
-    app.get('/templates/campaign-list-item', function(req, res){
-      res.render('sections/campaign-list-item', { title: '' });
-    });
+  });
 
-    app.get('/templates/campaign-detail', function(req, res){
-      res.render('sections/campaign-detail', { title: '' });
-    });
+  // Updating item
+  app.put('/campaign/:id', function(req, res){
+    console.log("Updating campaign " + req.params.id);
 
-    app.get('/campaign', function(req, res){
-      console.log("Listing campaigns");
+    // Retrieve form data
+    var id = req.params.id;
+    var name = req.param('name', null);
+    var description = req.param('description', null);
 
-      // TODO: Retrieve from real database
-      campaigns.push({ id : 1, name : 'Campaign 001', description : 'Fusce dapibus, tellus ac cursus commodo, tortor mauris' });
-      campaigns.push({ id : 2, name : 'Campaign 002', description : 'Fusce dapibus, tellus ac cursus commodo, tortor mauris' });
-      campaigns.push({ id : 3, name : 'Campaign 003', description : 'Fusce dapibus, tellus ac cursus commodo, tortor mauris' });
-      campaigns.push({ id : 4, name : 'Campaign 004', description : 'Fusce dapibus, tellus ac cursus commodo, tortor mauris' });
-      campaigns.push({ id : 5, name : 'Campaign 005', description : 'Fusce dapibus, tellus ac cursus commodo, tortor mauris' });
-
-      res.json(campaigns);
-    });
-
-    // Insert a new item
-    app.post('/campaign', function(req, res){
-      console.log("Creating a new campaign");
-
-      // Retrieve form data
-      var name = req.param('name', null);
-      var description = req.param('description', null);
-
-      // TODO: Insert a real database entry
-      campaigns.push({
-          id: campaigns.length + 1,
-          name: name,
-          dscription: description
-      });
-
-    });
-
-    // Updating item
-    app.put('/campaign/:id', function(req, res){
-      console.log("Updating campaign " + req.params.id);
-
-      // Retrieve form data
-      var id = req.params.id;
-      var name = req.param('name', null);
-      var description = req.param('description', null);
-
-      // TODO: Update a real database entry
-      var i;
-      for(i = 0, len = campaigns.length; i < len; ++i) {
-        if(campaigns[i].id === id) {
-          campaigns[i].name = name;
-          campaigns[i].dscription = description;
-          break;
-        }
+    // TODO: Update a real database entry
+    var i;
+    for(i = 0, len = campaigns.length; i < len; ++i) {
+      if(campaigns[i].id === id) {
+        campaigns[i].name = name;
+        campaigns[i].dscription = description;
+        break;
       }
+    }
 
-    });
+  });
 
-    // Delete a item
-    app.del('/campaign/:id', function(req, res){
-      console.log("Deleting campaign " + req.params.id);
+  // Delete a item
+  app.del('/campaign/:id', function(req, res){
+    console.log("Deleting campaign " + req.params.id);
 
-      // TODO: Delete real database entry
-    });
+    // TODO: Delete real database entry
+  });
 
 };
--- routes/report.js
+++ routes/report.js
@@ -5,82 +5,82 @@
 
 module.exports = function(app){
 
-    // TODO: Transform to database entity
-    var reports = [];
+  // TODO: Transform to database entity
+  var reports = [];
 
-    // Templates
-    app.get('/templates/report-list', function(req, res){
-      res.render('sections/report-list', { title: '' });
+  // Templates
+  app.get('/templates/report-list', function(req, res){
+    res.render('sections/report-list', { title: '' });
+  });
+
+  app.get('/templates/report-list-item', function(req, res){
+    res.render('sections/report-list-item', { title: '' });
+  });
+
+  app.get('/templates/report-detail', function(req, res){
+    res.render('sections/report-detail', { title: '' });
+  });
+
+  app.get('/report', function(req, res){
+    console.log("Listing reports");
+
+    // TODO: Retrieve from real database
+    reports.push({ id : 1, name : 'Report 001', description : 'Fusce dapibus, tellus ac cursus commodo, tortor mauris', email : 'maillist1@corp.com' });
+    reports.push({ id : 2, name : 'Report 002', description : 'Fusce dapibus, tellus ac cursus commodo, tortor mauris', email : 'maillist2@corp.com' });
+    reports.push({ id : 3, name : 'Report 003', description : 'Fusce dapibus, tellus ac cursus commodo, tortor mauris', email : 'maillist3@corp.com' });
+    reports.push({ id : 4, name : 'Report 004', description : 'Fusce dapibus, tellus ac cursus commodo, tortor mauris', email : 'maillist4@corp.com' });
+    reports.push({ id : 5, name : 'Report 005', description : 'Fusce dapibus, tellus ac cursus commodo, tortor mauris', email : 'maillist5@corp.com' });
+    reports.push({ id : 6, name : 'Report 006', description : 'Fusce dapibus, tellus ac cursus commodo, tortor mauris', email : 'maillist6@corp.com' });
+    reports.push({ id : 7, name : 'Report 007', description : 'Fusce dapibus, tellus ac cursus commodo, tortor mauris', email : 'maillist7@corp.com' });
+    reports.push({ id : 8, name : 'Report 008', description : 'Fusce dapibus, tellus ac cursus commodo, tortor mauris', email : 'maillist8@corp.com' });
+    reports.push({ id : 9, name : 'Report 009', description : 'Fusce dapibus, tellus ac cursus commodo, tortor mauris', email : 'maillist9@corp.com' });
+
+    res.json(reports);
+  });
+
+  // Insert a new item
+  app.post('/report', function(req, res){
+    console.log("Creating a new report");
+
+    // Retrieve form data
+    var name = req.param('name', null);
+    var description = req.param('description', null);
+
+    // TODO: Insert a real database entry
+    reports.push({
+      id: reports.length + 1,
+      name: name,
+      dscription: description
     });
 
-    app.get('/templates/report-list-item', function(req, res){
-      res.render('sections/report-list-item', { title: '' });
-    });
+  });
 
-    app.get('/templates/report-detail', function(req, res){
-      res.render('sections/report-detail', { title: '' });
-    });
+  // Updating item
+  app.put('/report/:id', function(req, res){
+    console.log("Updating report " + req.params.id);
 
-    app.get('/report', function(req, res){
-      console.log("Listing reports");
+    // Retrieve form data
+    var id = req.params.id;
+    var name = req.param('name', null);
+    var description = req.param('description', null);
 
-      // TODO: Retrieve from real database
-      reports.push({ id : 1, name : 'Report 001', description : 'Fusce dapibus, tellus ac cursus commodo, tortor mauris', email : 'maillist1@corp.com' });
-      reports.push({ id : 2, name : 'Report 002', description : 'Fusce dapibus, tellus ac cursus commodo, tortor mauris', email : 'maillist2@corp.com' });
-      reports.push({ id : 3, name : 'Report 003', description : 'Fusce dapibus, tellus ac cursus commodo, tortor mauris', email : 'maillist3@corp.com' });
-      reports.push({ id : 4, name : 'Report 004', description : 'Fusce dapibus, tellus ac cursus commodo, tortor mauris', email : 'maillist4@corp.com' });
-      reports.push({ id : 5, name : 'Report 005', description : 'Fusce dapibus, tellus ac cursus commodo, tortor mauris', email : 'maillist5@corp.com' });
-      reports.push({ id : 6, name : 'Report 006', description : 'Fusce dapibus, tellus ac cursus commodo, tortor mauris', email : 'maillist6@corp.com' });
-      reports.push({ id : 7, name : 'Report 007', description : 'Fusce dapibus, tellus ac cursus commodo, tortor mauris', email : 'maillist7@corp.com' });
-      reports.push({ id : 8, name : 'Report 008', description : 'Fusce dapibus, tellus ac cursus commodo, tortor mauris', email : 'maillist8@corp.com' });
-      reports.push({ id : 9, name : 'Report 009', description : 'Fusce dapibus, tellus ac cursus commodo, tortor mauris', email : 'maillist9@corp.com' });
-
-      res.json(reports);
-    });
-
-    // Insert a new item
-    app.post('/report', function(req, res){
-      console.log("Creating a new report");
-
-      // Retrieve form data
-      var name = req.param('name', null);
-      var description = req.param('description', null);
-
-      // TODO: Insert a real database entry
-      reports.push({
-          id: reports.length + 1,
-          name: name,
-          dscription: description
-      });
-
-    });
-
-    // Updating item
-    app.put('/report/:id', function(req, res){
-      console.log("Updating report " + req.params.id);
-
-      // Retrieve form data
-      var id = req.params.id;
-      var name = req.param('name', null);
-      var description = req.param('description', null);
-
-      // TODO: Update a real database entry
-      var i;
-      for(i = 0, len = reports.length; i < len; ++i) {
-        if(reports[i].id === id) {
-          reports[i].name = name;
-          reports[i].dscription = description;
-          break;
-        }
+    // TODO: Update a real database entry
+    var i;
+    for(i = 0, len = reports.length; i < len; ++i) {
+      if(reports[i].id === id) {
+        reports[i].name = name;
+        reports[i].dscription = description;
+        break;
       }
+    }
 
-    });
+  });
 
-    // Delete a item
-    app.del('/report/:id', function(req, res){
-      console.log("Deleting report " + req.params.id);
+  // Delete a item
+  app.del('/report/:id', function(req, res){
+    console.log("Deleting report " + req.params.id);
 
-      // TODO: Delete real database entry
-    });
+    // TODO: Delete real database entry
+  });
 
 };
